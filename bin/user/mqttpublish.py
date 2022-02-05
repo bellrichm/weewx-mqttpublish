e4fc8449205720832bfd8b088403fc3c43d2cf17
Rich Bell <bellrichm@gmail.com>
2022-02-05 07:38:27 -0500

    do not require ca_certs

--- bin/user/mqttpublish.py
+++ bin/user/mqttpublish.py
@@ -386,8 +386,6 @@ class MQTTPublish(object):
             pass
 
         ca_certs = tls_dict.get('ca_certs')
-        if ca_certs is None:
-            raise ValueError("'ca_certs' is required.")
 
         valid_cert_reqs = valid_cert_reqs.get(tls_dict.get('certs_required', 'required'))
         if valid_cert_reqs is None:
