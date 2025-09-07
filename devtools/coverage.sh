5904b9fcf42cf16582c1c50c28cf51d4fd780a8b
Rich Bell <bellrichm@gmail.com>
2025-09-07 13:48:02 -0400

    coverage devtool/script

---
+++ devtools/coverage.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+#    Copyright (c) 2025 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+source ./devtools/python_versions.sh
+
+export PYENV_VERSION=$weewx_default_python_version
+PYTHONPATH=bin:../weewx/src coverage run --branch -m pytest bin/user/tests/unit; 
+
+coverage html --include bin/user/mqttpublish.py
